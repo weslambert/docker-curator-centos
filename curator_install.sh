70c1ddb886986ce446063781a0a5fe7426e6152b
weslambert <wlambertts@gmail.com>
2017-07-06 13:52:18 -0400

    build until image is pushed

--- curator_install.sh
+++ curator_install.sh
@@ -5,7 +5,9 @@ URL="https://github.com/weslambert/$REPO.git"
 git clone $URL
 echo "export DOCKER_CONTENT_TRUST=1" >> /etc/profile.d/securityonion-docker.sh
 export DOCKER_CONTENT_TRUST=1
-docker pull wlambert/so-curator
+#docker pull wlambert/so-curator
+cd $REPO
+docker build -t so-curator .
 mkdir -p /etc/curator/config
 mkdir -p /etc/curator/action
 mkdir -p /var/log/curator
